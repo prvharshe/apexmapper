49b4b2df377512a37e3f4b7c6766daff7b76b83a
Suyash Talwar <42096700+talwarsuyash@users.noreply.github.com>
2021-11-18 23:45:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+showMaps = maps => {
+  const mapsDiv = document.querySelector('#rick-and-morty-maps');
+  console.log(mapsDiv);
+  //maps.forEach(map => {
+	  	console.log(maps);
+    const mapElement = document.createElement('p');
+	console.log(maps.battle_royale.current.map);
+    mapElement.innerText = maps.battle_royale.current.map + ' for ' + maps.battle_royale.current.remainingMins+ ' mins';
+    
+    mapsDiv.append(mapElement);
+  //});
+}
+
+fetch('https://api.mozambiquehe.re/maprotation?version=2&auth=j0m9K0gLd0kxEY3ztzZ7')
+   .then(response => response.json())
+   .then(maps => showMaps(maps));
+   
+   
